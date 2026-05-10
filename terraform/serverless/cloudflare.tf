7dfe85ce8dcb443990d4f47e724965ccefafefd3
Ubuntu <ubuntu@ip-172-31-65-219.ec2.internal>
2026-05-10 16:22:32 +0000

    updatedcloudflare2.tf

--- terraform/serverless/cloudflare.tf
+++ terraform/serverless/cloudflare.tf
@@ -67,7 +67,7 @@ resource "cloudflare_zero_trust_access_application" "private_tf" {
   type       = "self_hosted"
 
   session_duration           = var.cloudflare_session_duration
-  auto_redirect_to_identity  = false
+  auto_redirect_to_identity  = true
   http_only_cookie_attribute = false
 }
 
