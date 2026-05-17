32d9e98ec7a515fae7cec64ca0cc2fc519440805
Ubuntu <ubuntu@ip-172-31-65-219.ec2.internal>
2026-05-17 09:01:34 +0000

    changed cloudflare session duration

--- terraform/serverless/cloudflare.tf
+++ terraform/serverless/cloudflare.tf
@@ -66,7 +66,7 @@ resource "cloudflare_zero_trust_access_application" "private_tf" {
   domain     = "${var.private_subdomain}.${var.domain}"
   type       = "self_hosted"
 
-  session_duration           = var.cloudflare_session_duration
+  session_duration           = "0s"
   auto_redirect_to_identity  = true
   http_only_cookie_attribute = false
   allowed_idps               = [cloudflare_zero_trust_access_identity_provider.okta_tf.id]
